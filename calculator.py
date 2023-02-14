985b6868f7db55607fec7f98093710ceb60bc207
Fabricio Junior <fabriciojosejunior@gmail.com>
2023-02-14 12:28:44 -0300

    Adding Style & Display

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
 import sys
 from os import system
 
-from PyQt5.QtWidgets import (QApplication, QGridLayout, QMainWindow,
-                             QPushButton, QWidget)
+from PyQt5.QtWidgets import (QApplication, QGridLayout, QLineEdit, QMainWindow,
+                             QPushButton, QSizePolicy, QWidget)
 
 system('cls')
 
@@ -16,6 +16,17 @@ class Calculator(QMainWindow):
         self.setFixedSize(600, 600)
         self.cw = QWidget()
         self.grid = QGridLayout(self.cw)
+        self.list_operators = ['(', '+', '-', '/', '*', '%']
+        self.setStyleSheet('background: #161829')
+
+        # Adding a display
+        self.display = QLineEdit()
+        self.grid.addWidget(self.display, 0, 0, 1, 5)
+        self.display.setDisabled(True)
+        self.display.setStyleSheet(
+            'background: #212133; color: #fff; font-size: 30px; border-radius: 20px;')
+        self.display.setSizePolicy(
+            QSizePolicy.Preferred, QSizePolicy.Expanding)
 
 
 if __name__ == '__main__':
