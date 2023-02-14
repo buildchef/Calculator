69c4fc4c2c93c87241163ea4215d43ffaad37e39
Fabricio Junior <fabriciojosejunior@gmail.com>
2023-02-14 12:52:45 -0300

    Adding buttons

--- calculator.py
+++ calculator.py
@@ -28,6 +28,58 @@ class Calculator(QMainWindow):
         self.display.setSizePolicy(
             QSizePolicy.Preferred, QSizePolicy.Expanding)
 
+        # Adding buttons
+        # first line
+        self.add_btn(QPushButton('7'), 1, 0, 1, 1)
+        self.add_btn(QPushButton('8'), 1, 1, 1, 1)
+        self.add_btn(QPushButton('9'), 1, 2, 1, 1)
+        self.add_btn(QPushButton('+'), 1, 3, 1, 1)
+        self.add_btn(QPushButton('C'), 1, 4, 1, 1)
+
+        # second line
+        self.add_btn(QPushButton('4'), 2, 0, 1, 1)
+        self.add_btn(QPushButton('5'), 2, 1, 1, 1)
+        self.add_btn(QPushButton('6'), 2, 2, 1, 1)
+        self.add_btn(QPushButton('-'), 2, 3, 1, 1)
+        self.add_btn(QPushButton('<-'), 2, 4, 1, 1)
+
+        # third line
+        self.add_btn(QPushButton('1'), 3, 0, 1, 1)
+        self.add_btn(QPushButton('2'), 3, 1, 1, 1)
+        self.add_btn(QPushButton('3'), 3, 2, 1, 1)
+        self.add_btn(QPushButton('/'), 3, 3, 1, 1)
+        self.add_btn(QPushButton('()'), 3, 4, 1, 1)
+
+        # fourth     line
+        self.add_btn(QPushButton('.'), 4, 0, 1, 1)
+        self.add_btn(QPushButton('0'), 4, 1, 1, 1)
+        self.add_btn(QPushButton('%'), 4, 2, 1, 1)
+        self.add_btn(QPushButton('*'), 4, 3, 1, 1)
+        self.add_btn(QPushButton('='), 4, 4, 1, 1)
+
+        self.setCentralWidget(self.cw)
+
+    # Function to create buttons
+
+    def add_btn(self, btn, row, col, rowspan, colspan, function=None, style=None):
+        self.grid.addWidget(btn, row, col, rowspan, colspan)
+
+        if not function:
+            btn.clicked.connect(lambda: self.display.setText(
+                self.display.text() + btn.text()))
+
+        else:
+            btn.clicked.connect(function)
+
+        if not style:
+            btn.setStyleSheet(
+                'background: #212133; color: #fff; border: 2px solid #413b66; font-size: 30px; border-radius: 20px')
+
+        else:
+            btn.setStyleSheet(style)
+
+        btn.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+
 
 if __name__ == '__main__':
     qt = QApplication(sys.argv)
