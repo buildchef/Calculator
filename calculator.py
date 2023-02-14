c9771a834f3cd382de04b00f5813365a9c105f83
Fabricio Junior <fabriciojosejunior@gmail.com>
2023-02-14 12:20:19 -0300

    Creating the program window

--- calculator.py
+++ calculator.py
@@ -5,3 +5,21 @@ from PyQt5.QtWidgets import (QApplication, QGridLayout, QMainWindow,
                              QPushButton, QWidget)
 
 system('cls')
+
+# Creatting the Calculator class
+
+
+class Calculator(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('Calculator')
+        self.setFixedSize(600, 600)
+        self.cw = QWidget()
+        self.grid = QGridLayout(self.cw)
+
+
+if __name__ == '__main__':
+    qt = QApplication(sys.argv)
+    calc = Calculator()
+    calc.show()
+    qt.exec_()
